4560ad85f988ade017e47e011a8e9fd7ae668b14
ishiyama <hiroyuki293@icloud.com>
2024-12-04 23:44:29 +0900

    [add]画像保存multerの使用

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,40 @@
 import express from 'express';
+import {PrismaClient} from '@prisma/client';
+import multer from 'multer';
+import cors from 'cors';
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 3080;
+const upload = multer({dest:'../../public/uploads'})
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
   res.send('Hello from Express with TypeScript!!!!');
 });
+
+app.post('/api/saveProfile',upload.single('userIcon'),async(req,res)=>{
+  try{
+    const {userName} =req.body;
+    const  userIcon = req.file
+console.log(req.body)
+    if(!userName || !userIcon){
+       res.status(400).json({error:'userNameとuserIconは必須です'})
+       return
+    }
+
+    const saveProfile =await prisma.users.create({
+      data:{user_name:userName,icon_url:userIcon.path}
+    })
+  res.status(201).json({message:'保存成功',profile:saveProfile});
+  }catch(error){
+    console.log('データ保存エラー:',error);
+     res.status(500).json({error:'サーバーエラー'});
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
--- src/components/Profile/UserIcon.tsx
+++ src/components/Profile/UserIcon.tsx
@@ -1,9 +1,14 @@
 
 
 import { Avatar, Box } from '@mui/material';
-import { useRef, useState } from 'react';
+import {  useRef, useState } from 'react';
+import {  UseFormSetValue } from 'react-hook-form';
 
-const UserIcon = () => {
+type AvatarDataProps ={
+  setValue:UseFormSetValue<any>
+}
+
+const UserIcon= ({setValue}:AvatarDataProps) => {
 
   const fileInputRef = useRef<HTMLInputElement | null>(null)
   const [icon ,setIcon] = useState<string | null  >(null)
@@ -12,13 +17,19 @@ const UserIcon = () => {
   const files = fileInputRef.current?.files
   if(!files) return
   const file = files[0];
+  setValue('userIcon',file)
   const reader = new FileReader();
   reader.readAsDataURL(file);
   reader.onload = (e) =>{
     setIcon(String(e.target?.result))
+    // console.log(icon)
   }
 
   }
+
+
+
+  
 
   const fileUpLoad =() =>{
     fileInputRef.current?.click()
@@ -32,6 +43,7 @@ const UserIcon = () => {
   />
   <input type="file" 
   ref={fileInputRef}
+  
   onChange={handleInput}
   style={{   
     display:'none'
